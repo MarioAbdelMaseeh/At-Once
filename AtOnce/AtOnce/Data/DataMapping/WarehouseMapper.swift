b81004d6cc27b0bd26bbcd5f770484f788d278eb
Mario <90201555+MarioAbdelMaseeh@users.noreply.github.com>
2025-06-21 11:10:31 +0300

    fix ui

--- AtOnce/AtOnce/Data/DataMapping/WarehouseMapper.swift
+++ AtOnce/AtOnce/Data/DataMapping/WarehouseMapper.swift
@@ -7,6 +7,6 @@
 
 extension WarehouseDTO{
     func toEntity()->Warehouse{
-        return Warehouse(id: self.id ?? 1, name: self.name ?? "", address: self.address ?? "", imageUrl: self.imageUrl ?? "https://media.istockphoto.com/id/814423752/photo/eye-of-model-with-colorful-art-make-up-close-up.jpg?s=1024x1024&w=is&k=20&c=NQtm4v1Uzp2luv-6f3qORcq9pDtz2H56p8g9Xix8cY0=", minimumPrice: self.minmumPrice ?? 300, delivaryRate: self.delivaryRate ?? "daily", governate: self.governate ?? "")
+        return Warehouse(id: self.id ?? 1, name: self.name ?? "", address: self.address ?? "", imageUrl: self.imageUrl ?? "https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", minimumPrice: self.minmumPrice ?? 300, delivaryRate: self.delivaryRate ?? "daily", governate: self.governate ?? "")
     }
 }
