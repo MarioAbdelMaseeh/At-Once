27f75fbc07e2d88e5c154ea2d110350c69b97ff3
imanmahmoud <emanmhanafi@gmail.com>
2025-06-22 19:03:41 +0300

    coomit

--- AtOnce/AtOnce/Presentation/Login/LoginScreen.swift
+++ AtOnce/AtOnce/Presentation/Login/LoginScreen.swift
@@ -11,8 +11,10 @@ import SwiftUI
 struct LoginScreen: View {
     @StateObject var viewModel: LoginScreenViewModel
     
-    @State private var email = ""
-    @State private var password = ""
+//    @State private var email = ""
+//    @State private var password = ""
+    @State var showErrorAlert : Bool = false
+    @State var ShowSuccessAlert : Bool = false
     @State private var isPasswordVisible = false
     let buttonLabel = NSLocalizedString("login", comment: "")
     
@@ -22,7 +24,7 @@ struct LoginScreen: View {
 
     var body: some View {
         NavigationStack {
-            VStack/*(spacing: 20)*/ {
+            VStack(/*alignment:.leading ,spacing: 20*/) {
                 //Spacer(minLength: 10)
 
                 
@@ -43,10 +45,31 @@ struct LoginScreen: View {
                 }.padding(.bottom,32)
 
               
-                TextFieldComponent(title: NSLocalizedString("email", comment: ""), text: $email).padding(.bottom, 16)
+                TextFieldComponent(title: NSLocalizedString("email", comment: ""), text: $viewModel.email)
+//                    .padding(.bottom, 16)
+                if let emailError = viewModel.emailError {
+                    Text(emailError)
+                        .foregroundColor(.red)
+                        .font(.caption)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                       // .padding(.bottom, 12)
+                }
+                                    
+                    
                 
                
-                PasswordFieldComponent(title:NSLocalizedString("password", comment: ""),isPasswordVisible: $isPasswordVisible, password: $password)
+                PasswordFieldComponent(title:NSLocalizedString("password", comment: ""),isPasswordVisible: $isPasswordVisible, password: $viewModel.password)
+                    .padding(.top,16)
+                
+                if let passwordError = viewModel.passwordError {
+                    Text(passwordError)
+                        .foregroundColor(.red)
+                        .font(.caption)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        //.padding(.bottom, 12)
+                }
+                                    
+                
 
                
                 HStack {
@@ -61,7 +84,7 @@ struct LoginScreen: View {
 
                 
                 LargeButtonComponent(label: buttonLabel){
-                    viewModel.login(email: email, password: password)
+                    viewModel.login()
                 }
 
                 Spacer().frame(height: 80)
@@ -89,6 +112,25 @@ struct LoginScreen: View {
                 
             }
             .padding()
+            .alert(isPresented: Binding<Bool>(
+                get: { viewModel.loginSuccess != nil },
+                set: { _ in viewModel.loginSuccess = nil }
+            )) {
+                if viewModel.loginSuccess == true {
+                    return Alert(
+                        title: Text("Login Successful"),
+                        message: Text("Welcome back!"),
+                        dismissButton: .default(Text("OK"))
+                    )
+                } else {
+                    return Alert(
+                        title: Text("Login Failed"),
+                        message: Text(viewModel.errorMessage ?? "Unknown error"),
+                        dismissButton: .default(Text("Ok"))
+                    )
+                }
+            }
+                            
         }
         
     }
