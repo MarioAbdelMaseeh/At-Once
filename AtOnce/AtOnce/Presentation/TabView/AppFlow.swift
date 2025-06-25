372c00808834de7ba373ffaf7062887783e84563
imanmahmoud <emanmhanafi@gmail.com>
2025-06-26 01:32:40 +0300

    rrr

--- AtOnce/AtOnce/Presentation/TabView/AppFlow.swift
+++ AtOnce/AtOnce/Presentation/TabView/AppFlow.swift
@@ -10,7 +10,7 @@ enum AppFlow {
     case login
     case main
 }
-enum OutOfTabDestination: Hashable {
+enum OutOfTabDestination: Hashable , Equatable{
     case profile
     case store(id: Int)
     case profileInfo(CachedPharmacy)
